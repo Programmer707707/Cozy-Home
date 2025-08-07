033c61896f6e582ae9ada986d00a5dc8de65f9bc
Programmer707707 <toxirovsherzod707@gmail.com>
2025-08-07 22:25:55 +0500

    Loader has been added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ConfigProvider, theme } from 'antd';
 import { useThemeStore } from './stores/themeStore';
+import Loader from './components/Loader';
+
 
 const Home = lazy(() => import('./pages/Home'));
 const About = lazy(() => import('./pages/About'));
@@ -12,6 +15,17 @@ const Shop = lazy(() => import('./pages/Shop'));
 function App() {
   const isDark = useThemeStore((state) => state.isDark);
 
+  const [showLoader, setShowLoader] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowLoader(false);
+    }, 1000); 
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+
   return (
     <ConfigProvider
       theme={{
@@ -20,13 +34,17 @@ function App() {
     >
       <div className={`min-h-screen transition-colors duration-700 ease-in-out ${isDark ? 'bg-black text-white' : 'bg-[#FCFAF7] text-black'}`}>
         <Router>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/shop" element={<Shop />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
+          <Suspense fallback={<Loader />}>
+            {showLoader ? (
+              <Loader />
+            ) : (
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/shop" element={<Shop />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            )}
           </Suspense>
         </Router>
       </div>
